5ec77c355746b3c8d9343ee3af83c43b3e3a7790
shailesh shinde <shinde.shail@gmail.com>
2017-01-17 11:14:41 +0530

    adding intitial files

---
+++ src/test/java/com/bank/AppTest.java
@@ -0,0 +1,16 @@
+package test.java.com.bank;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		Assert.assertTrue("JUnit working greatly....", true);
+		//fail("Not yet implemented");
+		System.out.println("Junit run successfully...");
+	}
+}
